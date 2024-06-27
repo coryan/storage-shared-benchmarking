2eab3e166e9f8cbe7ece493d386d78eb39b806c4
Carlos O'Ryan <coryan@google.com>
2024-06-27 13:03:28 +0000

    Start splitting

--- maxt/cpp/maxt.cc
+++ maxt/cpp/maxt.cc
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+import maxt;
+
 #include <google/cloud/grpc_options.h>
 #include <google/cloud/internal/build_info.h>
 #include <google/cloud/internal/compiler_info.h>
@@ -74,10 +76,6 @@ namespace {
 
 using namespace std::literals;
 
-auto constexpr kKB = 1'000;
-auto constexpr kMB = kKB * kKB;
-auto constexpr kKiB = 1024;
-auto constexpr kMiB = kKiB * kKiB;
 auto constexpr kRandomDataSize = 32 * kMiB;
 
 auto constexpr kAppName = "maxt"sv;
@@ -105,13 +103,8 @@ auto constexpr kMemoryHistogramUnit = "1{memory}";
 auto constexpr kVersion = "1.2.0";
 auto constexpr kSchema = "https://opentelemetry.io/schemas/1.2.0";
 
-auto constexpr kDefaultIterations = 1'000'000;
-auto constexpr kDefaultSampleRate = 0.05;
-
 namespace gc = ::google::cloud;
 using dseconds = std::chrono::duration<double, std::ratio<1>>;
-
-boost::program_options::variables_map parse_args(int argc, char* argv[]);
 
 using histogram_ptr =
     opentelemetry::nostd::shared_ptr<opentelemetry::metrics::Histogram<double>>;
@@ -911,11 +904,6 @@ auto make_async_dp(boost::program_options::variables_map const& vm) {
       async_options(vm, "dp-", "google-c2p:///storage.googleapis.com"));
 }
 
-auto constexpr kJson = "JSON"sv;
-auto constexpr kGrpcCfe = "GRPC+CFE"sv;
-auto constexpr kGrpcDp = "GRPC+DP"sv;
-auto constexpr kAsyncGrpcCfe = "ASYNC+GRPC+CFE"sv;
-auto constexpr kAsyncGrpcDp = "ASYNC+GRPC+DP"sv;
 
 named_experiments make_experiments(
     boost::program_options::variables_map const& vm) {
@@ -1096,79 +1084,6 @@ std::unique_ptr<opentelemetry::metrics::MeterProvider> make_meter_provider(
                      kMemoryHistogramUnit, make_memory_histogram_boundaries());
 
   return provider;
-}
-
-boost::program_options::variables_map parse_args(int argc, char* argv[]) {
-  auto const cores = static_cast<int>(std::thread::hardware_concurrency());
-
-  namespace po = boost::program_options;
-  po::options_description desc(
-      "A simple publisher application with Open Telemetery enabled");
-  // The following empty line comments are for readability.
-  desc.add_options()                      //
-      ("help,h", "produce help message")  //
-      // Benchmark options
-      ("bucket", po::value<std::string>()->required(),
-       "the name of a Google Cloud Storage bucket. The benchmark uses this"
-       " bucket to upload and download objects and measures the latency.")  //
-      ("deployment", po::value<std::string>()->default_value("development"),
-       "a short string describing where the benchmark is deployed, e.g."
-       " development, or GKE, or GCE.")  //
-      ("iterations", po::value<int>()->default_value(kDefaultIterations),
-       "the number of iterations before exiting the test")  //
-      // Create enough workers to keep the available CPUs busy rea
-      ("worker-counts",
-       po::value<std::vector<int>>()->multitoken()->default_value(
-           {cores, 2 * cores}, std::format("[ {}, {} ]", cores, 2 * cores)),
-       "the object sizes used in the benchmark.")
-      // Create enough objects, such that every worker has at least one.
-      ("object-counts",
-       po::value<std::vector<int>>()->multitoken()->default_value(
-           {2 * cores}, std::format("[ {} ]", 2 * cores)),
-       "the object counts used in the benchmark.")
-      // Make the objects large enough to mask the TTFB delays.
-      ("object-sizes",
-       po::value<std::vector<std::int64_t>>()->multitoken()->default_value(
-           {256 * kMiB}, "[ 256MiB ]"),
-       "the object sizes used in the benchmark.")
-      // By default read the data only once. That is a fairly cold dataset,
-      // increase this number to simulate hotter datasets.
-      ("repeated-read-counts",
-       po::value<std::vector<int>>()->multitoken()->default_value({1}, "[ 1 ]"),
-       "read each object multiple times to simulate 'hot' data.")  //
-      ("experiments",
-       po::value<std::vector<std::string>>()->multitoken()->default_value(
-           {std::string(kJson), std::string(kGrpcCfe),
-            std::string(kAsyncGrpcCfe)},
-           std::format("[ {}, {}, {} ]", kJson, kGrpcCfe, kAsyncGrpcCfe)),
-       "the experiments used in the benchmark.")
-      //
-      ("runners", po::value<int>()->default_value(1),
-       "the number of runners to run in parallel.")
-      // gRPC configuration options
-      ("cfe-thread-pool", po::value<int>(), "CFE background thread pool.")  //
-      ("cfe-channels", po::value<int>(), "the number of CFE channels.")     //
-      ("dp-thread-pool", po::value<int>(), "DP background thread pool.")    //
-      ("dp-channels", po::value<int>(), "the number of DP channels.")       //
-      // Open Telemetry Processor options
-      ("project-id", po::value<std::string>()->required(),
-       "a Google Cloud Project id. The benchmark sends its results to this"
-       " project as Cloud Monitoring metrics and Cloud Trace traces.")  //
-      ("tracing-rate", po::value<double>()->default_value(kDefaultSampleRate),
-       "otel::BasicTracingRateOption value")  //
-      ("max-queue-size", po::value<std::size_t>()->default_value(1'000'000),
-       "set the max queue size for open telemetery")  //
-      ;
-
-  po::variables_map vm;
-  po::store(po::command_line_parser(argc, argv).options(desc).run(), vm);
-  if (vm.count("help") || argc == 1) {
-    std::cerr << "Usage: " << argv[0] << "\n";
-    std::cerr << desc << "\n";
-    std::exit(argc == 1 ? EXIT_FAILURE : EXIT_SUCCESS);
-  }
-  po::notify(vm);
-  return vm;
 }
 
 }  // namespace
